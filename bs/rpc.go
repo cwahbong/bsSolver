2293d4a1dd4ad5d6dc818f49cc094de6c25debbd
cwahbong <cw.ahbong@gmail.com>
2013-09-13 00:05:57 +0800

    Add rpc server handler.

---
+++ bs/rpc.go
@@ -0,0 +1,29 @@
+package bs
+
+import (
+	"github.com/gorilla/rpc"
+	"github.com/gorilla/rpc/json"
+	"net/http"
+)
+
+type bsService struct {}
+
+type bsSolveArgs struct {
+	Board [][]int `json:"board"`
+}
+
+type bsSolveReply struct {
+	Lines []Line `json:"lines"`
+}
+
+func (*bsService) Solve(request *http.Request, args *bsSolveArgs, reply *bsSolveReply) (err error) {
+	reply.Lines, _ = Solve(args.Board)
+	return
+}
+
+func RpcServer() *rpc.Server {
+	rpcServer := rpc.NewServer()
+	rpcServer.RegisterCodec(json.NewCodec(), "application/json")
+	rpcServer.RegisterService(new(bsService), "bs")
+	return rpcServer
+}
